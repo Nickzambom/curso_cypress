f2c847bf693a93e244f027494ee37bbedd31a818
Binho <nicholasnkzambom@gmail.com>
2024-09-02 16:26:28 -0300

    adicionado dep de gerar relatorio

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,13 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    video: true,
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/results',
+      overwrite: false,
+      html:true,
+      json: false,
+      timestamp: "mmddyyyy_HHMMss"}
   },
 });
--- cypress/e2e/CN002_LogarComUsuarioValido.cy.js
+++ cypress/e2e/CN002_LogarComUsuarioValido.cy.js
@@ -2,11 +2,10 @@ describe('Realizar Login com usuario valido e clicar em falar', ()=> {
     beforeEach(()=> {
         //iniciar teste - adopet
         cy.visit('https://adopet-frontend-cypress.vercel.app/');
+    })
+    it('realizar login e validar home',() =>{
         //validar campo de login
         cy.contains('a','Fazer login').click();
-    })
-
-    it('realizar login e validar home',() =>{
         //validar campo de login
         cy.get('input[id="email"').should('be.visible');
         cy.get('input[id="email"').type("nicholasnkzambom@gmail.com");
@@ -15,7 +14,7 @@ describe('Realizar Login com usuario valido e clicar em falar', ()=> {
         cy.get('input[id="pass"').type("NicholasNIC@12");
         //clicar no botão de entrar
         cy.contains('button','Entrar').click();
-        //validar pagina de login icon de email e home (header esta visivel
+        //validar pagina de login icon de email e home (header esta visivel)
         cy.get('.header__user').should('be.visible');
         //validar falar com responsavel - adopet
         cy.get(':nth-child(1) > .card__contact').should('be.visible');
